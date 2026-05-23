9ddd5189efd4c125efaa290d1717d01d0762faf0
Ayush Rawat <ayushrawat5324@gmail.com>
2026-05-22 21:15:46 -0500

    Create Spring Boot backend with health check API

---
+++ backend/cloudhire360-api/src/main/java/com/cloudhire360/cloudhire360_api/Cloudhire360ApiApplication.java
@@ -0,0 +1,13 @@
+package com.cloudhire360.cloudhire360_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudhire360ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloudhire360ApiApplication.class, args);
+	}
+
+}
---
+++ backend/cloudhire360-api/src/test/java/com/cloudhire360/cloudhire360_api/Cloudhire360ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudhire360.cloudhire360_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloudhire360ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
